febb604f71fc62db9f3031d6a12006fc6f98dc09
suraj <surajsharma@pm.me>
2020-11-24 12:54:53 +0530

    added page elements

--- client/src/App.js
+++ client/src/App.js
@@ -2,92 +2,121 @@ import React from "react";
 import "./styles.css";
 import { Graph } from "react-d3-graph";
 
-// graph object to data
-// graph is like this: {1:[2,3,4,5], 2:[6], 3:[6,7], 4:[7,8], 5:[8]}
-
-// const data = {
-//   nodes: [
-//     { id: "1" },
-//     { id: "2" },
-//     { id: "3" },
-//     { id: "4" },
-//     { id: "5" },
-//     { id: "6" },
-//     { id: "7" },
-//     { id: "8" }
-//   ],
-//   links: [
-//     { source: "1", target: "2" },
-//     { source: "1", target: "3" },
-//     { source: "1", target: "4" },
-//     { source: "1", target: "5" },
-//     { source: "2", target: "6" },
-//     { source: "3", target: "6" },
-//     { source: "3", target: "7" },
-//     { source: "4", target: "7" },
-//     { source: "4", target: "8" },
-//     { source: "5", target: "8" }
-//   ]
-// };
-
 const graphToData = (graph) => {
-  let k = [],
-    kv = [],
-    v = [],
-    nodes = [],
-    links = [];
-  Object.keys(graph).forEach((key) => k.push(parseInt(key)));
-  Object.values(graph).forEach((val) => v.push(val));
-  Object.values(graph).forEach((val) => val.forEach((vv) => kv.push(vv)));
-  let n = [...new Set(k.concat(kv))];
+    let k = [],
+        kv = [],
+        v = [],
+        nodes = [],
+        links = [];
+    Object.keys(graph).forEach((key) => k.push(parseInt(key)));
+    Object.values(graph).forEach((val) => v.push(val));
+    Object.values(graph).forEach((val) => val.forEach((vv) => kv.push(vv)));
+    let n = [...new Set(k.concat(kv))];
 
-  n.forEach((node, i) => {
-    nodes.push({ id: node });
-  });
+    n.forEach((node, i) => {
+        nodes.push({ id: node });
+    });
 
-  v.forEach((val) =>
-    val.forEach((vv, i) => links.push({ source: nodes[i].id, target: vv }))
-  );
+    v.forEach((val) =>
+        val.forEach((vv, i) => links.push({ source: nodes[i].id, target: vv }))
+    );
 
-  console.log({ nodes, links });
-  return { nodes, links };
+    return { nodes, links };
 };
 
-const graph = graphToData({
-  1: [2, 3, 4, 5],
-  2: [6],
-  3: [6, 7],
-  4: [7, 8],
-  5: [8]
+const newgraph = graphToData({
+    1: [2, 3, 4, 5],
+    2: [6],
+    3: [6, 7],
+    4: [7, 8],
+    5: [8],
 });
 
+const graph = {
+    1: [2, 3, 4, 5],
+    2: [6],
+    3: [6, 7],
+    4: [7, 8],
+    5: [8],
+};
 // the graph configuration, you only need to pass down properties
 // that you want to override, otherwise default ones will be used
 const myConfig = {
-  nodeHighlightBehavior: true,
-  node: {
-    color: "lightgreen",
-    size: 500,
-    highlightStrokeColor: "blue"
-  },
-  link: {
-    highlightColor: "lightblue"
-  },
-  directed: true,
-  automaticRearrangeAfterDropNode: true,
-  collapsible: true
+    nodeHighlightBehavior: true,
+    node: {
+        color: "lightgreen",
+        size: 500,
+        highlightStrokeColor: "blue",
+    },
+    link: {
+        highlightColor: "lightblue",
+    },
+    directed: true,
+    automaticRearrangeAfterDropNode: true,
+    collapsible: true,
+};
+
+// graph event callbacks
+const onClickGraph = function () {
+    console.log(`Clicked the graph background`);
+};
+
+const onClickNode = function (nodeId) {
+    console.log(`Clicked node ${nodeId}`);
+};
+
+const onDoubleClickNode = function (nodeId) {
+    console.log(`Double clicked node ${nodeId}`);
+};
+
+const onRightClickNode = function (event, nodeId) {
+    console.log(`Right clicked node ${nodeId}`);
+};
+
+const onMouseOverNode = function (nodeId) {
+    console.log(`Mouse over node ${nodeId}`);
+};
+
+const onMouseOutNode = function (nodeId) {
+    console.log(`Mouse out node ${nodeId}`);
+};
+
+const onClickLink = function (source, target) {
+    console.log(`Clicked link between ${source} and ${target}`);
+};
+
+const onRightClickLink = function (event, source, target) {
+    console.log(`Right clicked link between ${source} and ${target}`);
+};
+
+const onMouseOverLink = function (source, target) {
+    console.log(`Mouse over in link between ${source} and ${target}`);
+};
+
+const onMouseOutLink = function (source, target) {
+    console.log(`Mouse out link between ${source} and ${target}`);
 };
 
 const onNodePositionChange = function (nodeId, x, y) {
-  window.alert(
-    `Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`
-  );
+    console.log(
+        `Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`
+    );
 };
 
 export default function App() {
-  return (
-    <div className="App">
-      <Graph id="graph-id" data={graph} config={myConfig} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <textarea value={JSON.stringify(graph)} /> &nbsp;
+            <textarea value={JSON.stringify(graph)} />
+            <br />
+            <button>Get All Paths / Redraw Graph</button>
+            <Graph
+                id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+                data={newgraph}
+                config={myConfig}
+                onNodePositionChange={onNodePositionChange}
+            />
+            ;
+        </div>
+    );
 }
